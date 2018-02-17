5c1b3d1bbd75b458eb2ea0323f6b14565c51b163
Hai Huynh <Hai.Ba.Huynh@mgm-tp.com>
2018-02-18 00:50:28 +0700

    Add Post Component

--- notefe-app/frontend/src/app/app.component.ts
+++ notefe-app/frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'This is app adlaskdh;aksdjlakjsd;lkja;ds';
+  title = 'NHK Easy Reading App';
 }
